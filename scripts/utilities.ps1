6ccd0ba4a0e424e6d171ca75bb9cd7607e9cbe38
Petrik van der Velde <petrikvandervelde@gmail.com>
2021-01-21 01:05:43 +1300

    Get a coverage file for the unit tests

--- scripts/utilities.ps1
+++ scripts/utilities.ps1
@@ -116,5 +116,6 @@ function New-LocalBuild
 
     go build -a -installsuffix cgo -v -ldflags="-X github.com/calvinverse/service.provisioning.controller/internal/info.sha1=$sha1 -X github.com/calvinverse/service.provisioning.controller/internal/info.buildTime=$date -X github.com/calvinverse/service.provisioning.controller/internal/info.version=$version" -o $outputDir/controller.exe ./cmd
 
-    go test -cover ./... ./cmd
+    go test -cover -coverprofile="$outputDir/coverage.log" -v ./... ./cmd
+    go tool cover -html="$outputDir/coverage.log" -o "$outputDir/coverage.html"
 }
