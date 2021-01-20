59f4ddc641e3da0bed21f6a689e3885fc9bc8dba
Petrik van der Velde <petrikvandervelde@gmail.com>
2021-01-20 18:16:54 +1300

    Documentation comments

--- internal/health/health.go
+++ internal/health/health.go
@@ -1,15 +1,19 @@
 package health
 
-type HealthService interface {
-	Readiness() HealthStatus, error
+// Service defines a service that tracks the health of the application.
+type Service interface {
+	Readiness() (Status, error)
 
-	Liveliness() HealthStatus, error
+	Liveliness() (Status, error)
 }
 
-type HealthStatus struct {
-
+// Status stores the health status for the application.
+type Status struct {
 }
 
-type healthReporter {
+// CheckStatus stores the results of a health check.
+type CheckStatus struct {
+}
 
+type healthReporter struct {
 }
